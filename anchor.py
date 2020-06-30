19b1317ec9ffda0ddca40147615b715a2b221b30
csm-kr <csm8167@naver.com>
2020-06-30 10:28:04 +0900

    make anchor for yolov2

---
+++ anchor.py
@@ -0,0 +1,46 @@
+import numpy as np
+import torch
+
+
+def make_center_anchors(anchors_wh, grid_size=13):
+
+    grid_arange = np.arange(grid_size)
+    xx, yy = np.meshgrid(grid_arange, grid_arange)  # + 0.5  # grid center, [fmsize*fmsize,2]
+    m_grid = np.concatenate([np.expand_dims(xx, axis=-1), np.expand_dims(yy, -1)], axis=-1) + 0.5
+    # print(m_grid)
+    m_grid = m_grid
+    xy = torch.from_numpy(m_grid)
+
+    anchors_wh = np.array(anchors_wh)  # numpy 로 변경
+    wh = torch.from_numpy(anchors_wh)
+
+    xy = xy.view(13, 13, 1, 2).expand(13, 13, 5, 2).type(torch.float32)  # centor
+    wh = wh.view(1, 1, 5, 2).expand(13, 13, 5, 2).type(torch.float32)  # w, h
+    center_anchors = torch.cat([xy, wh], dim=3).cuda()
+    # cy cx w h
+    """
+    center_anchors[0][0]
+    tensor([[ 0.5000,  0.5000,  1.3221,  1.7314],
+            [ 0.5000,  0.5000,  3.1927,  4.0094],
+            [ 0.5000,  0.5000,  5.0559,  8.0989],
+            [ 0.5000,  0.5000,  9.4711,  4.8405],
+            [ 0.5000,  0.5000, 11.2364, 10.0071]], device='cuda:0')
+            
+    center_anchors[0][1]
+    tensor([[ 1.5000,  0.5000,  1.3221,  1.7314],
+            [ 1.5000,  0.5000,  3.1927,  4.0094],
+            [ 1.5000,  0.5000,  5.0559,  8.0989],
+            [ 1.5000,  0.5000,  9.4711,  4.8405],
+            [ 1.5000,  0.5000, 11.2364, 10.0071]], device='cuda:0')
+            
+    center_anchors[1][0]
+    tensor([[ 0.5000,  1.5000,  1.3221,  1.7314],
+            [ 0.5000,  1.5000,  3.1927,  4.0094],
+            [ 0.5000,  1.5000,  5.0559,  8.0989],
+            [ 0.5000,  1.5000,  9.4711,  4.8405],
+            [ 0.5000,  1.5000, 11.2364, 10.0071]], device='cuda:0')
+    
+    pytorch view has reverse index
+    """
+    return center_anchors
+
