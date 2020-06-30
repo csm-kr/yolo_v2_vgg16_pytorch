f7ef53c386afe029ce78974ad51d276c61df51da
csm-kr <csm8167@naver.com>
2020-06-30 10:25:11 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# YOLO V2 detection using vgg16
+
+re-implementation of yolo v2 detection 
+
+### Setting
+
+- Python 3.7
+- Pytorch 1.5.0
+- visdom
+- numpy 
+
+### Network
+
+- pretrained vgg 16 bn (torchvision.model)
+
+- conv 5_3
+
+- skip module
+
+- extra, final detection module 
+
+### Loss
+
+- xy centor loss 
+
+- wh ration loss 
+
+- confidence loss
+
+- no conf loss
+
+- classification loss 
+
+- whole loss is sum of those losses
+
+### Dataset
+
+for train 
+- voc 2007 train + voc 2012 train
+
+for test
+- voc 2007 test
+
+### evaluation 
+
+- mAP (metric for object detection using voc 2012 IOU:>0.5)
+
+- 57.3% not cover original darknet19 yolo v2 
+
+### training 
+
+- learning rate : 1e-4
+
+- optimizer : sgd
+
+- lr decay : 0 ~ 99 (1e-4), 100~149(1e-5)
+
+### experiments
+
+1. 
+
+2. 
+
+### Start Guide
+
+- for training
+
+### Result 
+
+- qualitative results
+
+- qualified results
+
---
+++ model.py
@@ -0,0 +1,85 @@
+import torch
+import torch.nn as nn
+from torchvision.models import vgg16_bn
+
+
+class YOLO_VGG_16(nn.Module):
+    def __init__(self, vgg=vgg16_bn(pretrained=True), num_classes=20):
+        super().__init__()
+
+        self.num_anchors = 5
+        self.num_classes = num_classes
+
+        self.middle_feature = nn.Sequential(*list(vgg.features.children())[:-1])  # after conv5_3 [512, 26, 26]
+
+        self.extra = nn.Sequential(nn.Conv2d(512, 512, 3, 1, 1),
+                                   nn.BatchNorm2d(512),
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(512, 512, 3, 1, 1),
+                                   nn.BatchNorm2d(512),
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(512, 512, 3, 1, 1),
+                                   nn.BatchNorm2d(512),
+                                   nn.ReLU(inplace=True),
+                                   nn.MaxPool2d(2, 2))
+
+        self.skip_module = nn.Sequential(nn.Conv2d(512, 64, 1, stride=1, padding=0),
+                                         nn.BatchNorm2d(64),
+                                         nn.ReLU(inplace=True))
+
+        self.final = nn.Sequential(nn.Conv2d(768, 1024, 3, padding=1),
+                                   nn.BatchNorm2d(1024),
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(1024, 256, 3, padding=1),
+                                   nn.BatchNorm2d(256),
+                                   nn.ReLU(inplace=True),
+                                   nn.Conv2d(256, self.num_anchors * (5 + self.num_classes), 1))  # anchor 5, class 20
+        self.init_conv2d()
+        print("num_params : ", self.count_parameters())
+
+    def init_conv2d(self):
+        for c in self.extra.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, mean=0.0, std=0.01)
+                nn.init.constant_(c.bias, 0.)
+
+        for c in self.skip_module.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, mean=0.0, std=0.01)
+                nn.init.constant_(c.bias, 0.)
+
+        for c in self.final.children():
+            if isinstance(c, nn.Conv2d):
+                nn.init.normal_(c.weight, mean=0.0, std=0.01)
+                nn.init.constant_(c.bias, 0.)
+
+    def forward(self, x):
+
+        x = self.middle_feature(x)              # after conv4_3, maxpooling
+        skip_x = self.skip_module(x)            # torch.Size([B, 512, 26, 26])--->  torch.Size([B, 64, 26, 26])
+
+        # --------------------- yolo v2 reorg layer ---------------------
+        skip_x = skip_x.view(-1, 64, 13, 2, 13, 2).contiguous()
+        skip_x = skip_x.permute(0, 3, 5, 1, 2, 4).contiguous()
+        skip_x = skip_x.view(-1, 256, 13, 13)   # torch.Size([B, 256, 13, 13])
+
+        x = self.extra(x)                       # torch.Size([B, 1024, 13, 13])
+        x = torch.cat([x, skip_x], dim=1)       # torch.Size([B, 1280, 13, 13])
+        x = self.final(x)                       # torch.Size([B, 125, 13, 13])
+        return x
+
+    def count_parameters(self):
+        return sum(p.numel() for p in self.parameters() if p.requires_grad)
+
+
+if __name__ == '__main__':
+    model = YOLO_VGG_16().cuda()
+    # model = nn.Sequential(*list(vgg16_bn(pretrained=True).features.children())[:-1]).cuda()
+    print(model)
+    image = torch.randn([1, 3, 416, 416]).cuda()
+    print(model(image).size())
+
+
+
+
+
